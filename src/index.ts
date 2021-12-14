dca45632c488fda2a061dd514f3cf0af2141dfd6
Paul Cothenet <pcothenet@gmail.com>
2021-12-13 17:08:57 -0800

    Set project_id

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-// const _ = require('lodash');
-// import Patch from '@patch-technology/patch';
 const { default: Patch } = require('@patch-technology/patch');
 
 const patch = Patch(process.env.PATCH_API_KEY);
 
+const DEFAULT_PROJECT_ID = 'pro_prod_a0bd405145cf44b744edcaba0a6ae399'; // Charm Industrial
+const PROJECT_ID = process.env.PROJECT_ID || DEFAULT_PROJECT_ID;
 const BIWEEKLY_AMOUNT_USD = Number(process.env.BIWEEKLY_AMOUNT_USD);
 
 async function placeOrder() {
   const order = await patch.orders.createOrder({
     total_price_cents_usd: BIWEEKLY_AMOUNT_USD * 100,
-    // project_id: "" // default to the project_id defined as Preferred on the dashboard
+    project_id: PROJECT_ID,
   });
   console.log(order);
 }
